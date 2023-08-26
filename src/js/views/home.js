80054ffeeff4346be419196547bbe34b62e94602
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-08-26 01:45:28 +0000

    :tada: Our async await fetch!

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,18 +5,22 @@ export const Home = () => {
 	
 	const [ heroes, setHeroes ] = useState([])
 	
-	useEffect( async ()=>{
+	useEffect( () => {
+
 		const ts = new Date()
-		fetch(`https://gateway.marvel.com/v1/public/characters?limit=100&ts=${ts}&apikey=${process.env.PUBLIC_KEY}&hash=${md5(ts+process.env.PRIVATE_KEY+process.env.PUBLIC_KEY)}`)
-			.then((resp) => {
-				return resp.json()
-			})
-			.then((superheroes)=>{
+
+		const buscarHeroes = async () => {
+			try{
+				let resp = await fetch(`https://gateway.marvel.com/v1/public/characters?limit=100&ts=${ts}&apikey=${process.env.PUBLIC_KEY}&hash=${md5(ts+process.env.PRIVATE_KEY+process.env.PUBLIC_KEY)}`)
+				let superheroes = await resp.json()
 				setHeroes(superheroes.data.results)
-			})
-			.catch((err)=>{
+			}catch(err){
 				console.log(err)
-			})
+			}
+		}
+
+		buscarHeroes()
+			
 	},[])
 	
 	return ( <div className="text-center mt-5">
@@ -27,9 +31,14 @@ export const Home = () => {
 		{
 			heroes.length != 0 &&
 			heroes.map( item => (
-				<h2 key={item.id}>
-					{item.name}
-				</h2>
+				<div className="card col-12">
+					<img src={item.thumbnail.path+"/standard_fantastic."+item.thumbnail.extension} className="card-img-top" alt={item.name} />
+					<div className="card-body">
+						<h5 className="card-title">{item.name}</h5>
+						<p className="card-text">{item.description}</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
 			) )
 		}
 	</div>
