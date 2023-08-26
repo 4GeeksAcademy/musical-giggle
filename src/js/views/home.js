444bc3dd2dcef8f73f704bd0782ff1f0714427ce
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-08-26 01:27:53 +0000

    :zap: Our first fetch!

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect } from "react";
+import * as md5 from "blueimp-md5"; 
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () => {
+	
+	const [ heroes, setHeroes ] = useState([])
+	
+	useEffect( async ()=>{
+		const ts = new Date()
+		fetch(`https://gateway.marvel.com/v1/public/characters?limit=100&ts=${ts}&apikey=${process.env.PUBLIC_KEY}&hash=${md5(ts+process.env.PRIVATE_KEY+process.env.PUBLIC_KEY)}`)
+			.then((resp) => {
+				return resp.json()
+			})
+			.then((superheroes)=>{
+				setHeroes(superheroes.data.results)
+			})
+			.catch((err)=>{
+				console.log(err)
+			})
+	},[])
+	
+	return ( <div className="text-center mt-5">
+		<h1> Marvel API request!</h1>
+		{	
+			heroes.length == 0 && <span> Loading ...</span>		
+		}
+		{
+			heroes.length != 0 &&
+			heroes.map( item => (
+				<h2 key={item.id}>
+					{item.name}
+				</h2>
+			) )
+		}
 	</div>
-);
+)};
